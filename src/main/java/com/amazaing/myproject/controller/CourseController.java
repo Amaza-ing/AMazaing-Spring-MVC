663b40f44a0b0b04356fed89384ab182d92752f4
Adrián Maza <adri_mv1@hotmail.com>
2026-05-08 18:14:25 +0200

    createCourse and saveCourse added to CourseController

--- src/main/java/com/amazaing/myproject/controller/CourseController.java
+++ src/main/java/com/amazaing/myproject/controller/CourseController.java
@@ -4,9 +4,8 @@ import com.amazaing.myproject.model.Course;
 import com.amazaing.myproject.service.CourseService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/courses")
@@ -41,5 +40,27 @@ public class CourseController {
         model.addAttribute("course", course);
 
         return "courses/detail";
+    }
+
+    @GetMapping("/new")
+    public String createCourse(Model model) {
+        model.addAttribute("pageTitle", "Crear nuevo curso");
+        model.addAttribute("course", new Course());
+
+        return "courses/form";
+    }
+
+    @PostMapping
+    public String saveCourse(
+            @ModelAttribute("course") Course course,
+            RedirectAttributes redirectAttributes) {
+
+        courseService.save(course);
+
+        redirectAttributes.addFlashAttribute(
+                "successMessage",
+                "Curso creado correctamente");
+
+        return "redirect:/courses";
     }
 }
