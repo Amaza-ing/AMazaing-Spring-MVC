69f80ecee6ed067939f0db581ffb8e3510075ffc
Adrián Maza <adri_mv1@hotmail.com>
2026-05-08 17:59:23 +0200

    save course added to CourseService

--- src/main/java/com/amazaing/myproject/service/CourseService.java
+++ src/main/java/com/amazaing/myproject/service/CourseService.java
@@ -3,16 +3,21 @@ package com.amazaing.myproject.service;
 import com.amazaing.myproject.model.Course;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CourseService {
 
-    private final List<Course> courses = List.of(
-            new Course(1L, "HTML & CSS", "Inicial", 20),
-            new Course(2L, "Java & POO", "Intermedio", 15),
-            new Course(3L, "Spring MVC", "Avanzado", 25)
-    );
+    private final List<Course> courses = new ArrayList<>();
+
+    private Long nextId = 4L;
+
+    public CourseService() {
+        courses.add(new Course(1L, "HTML & CSS", "Inicial", 20));
+        courses.add(new Course(2L, "Java & POO", "Intermedio", 15));
+        courses.add(new Course(3L, "Spring MVC", "Avanzado", 25));
+    }
 
     public List<Course> findAll() {
         return courses;
@@ -23,5 +28,11 @@ public class CourseService {
                 .filter(course -> course.getId().equals(id))
                 .findFirst()
                 .orElse(null);
+    }
+
+    public void save(Course course) {
+        course.setId(nextId);
+        courses.add(course);
+        nextId++;
     }
 }
