a1ca6c25db21acd56c5558201dba0416efd51e6d
Adrián Maza <adri_mv1@hotmail.com>
2026-05-08 17:52:03 +0200

    change int to Integer, add empty constructor and setters to Course model

--- src/main/java/com/amazaing/myproject/model/Course.java
+++ src/main/java/com/amazaing/myproject/model/Course.java
@@ -5,9 +5,12 @@ public class Course {
     private Long id;
     private String title;
     private String level;
-    private int durationInHours;
+    private Integer durationInHours;
 
-    public Course(Long id, String title, String level, int durationInHours) {
+    public Course() {
+    }
+
+    public Course(Long id, String title, String level, Integer durationInHours) {
         this.id = id;
         this.title = title;
         this.level = level;
@@ -26,7 +29,23 @@ public class Course {
         return level;
     }
 
-    public int getDurationInHours() {
+    public Integer getDurationInHours() {
         return durationInHours;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public void setDurationInHours(Integer durationInHours) {
+        this.durationInHours = durationInHours;
     }
 }
