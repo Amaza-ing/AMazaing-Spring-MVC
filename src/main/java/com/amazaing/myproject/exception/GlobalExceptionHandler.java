a4eee63ca962913c42c94b7a17f86721388b0f6d
Adrián Maza <adri_mv1@hotmail.com>
2026-05-09 14:26:02 +0200

    404 and 500 behaviour fixed

--- src/main/java/com/amazaing/myproject/exception/GlobalExceptionHandler.java
+++ src/main/java/com/amazaing/myproject/exception/GlobalExceptionHandler.java
@@ -1,13 +1,18 @@
 package com.amazaing.myproject.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CourseNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public String handleCourseNotFoundException(
             CourseNotFoundException exception,
             Model model) {
@@ -19,7 +24,21 @@ public class GlobalExceptionHandler {
         return "error/course-not-found";
     }
 
+    @ExceptionHandler({
+            NoHandlerFoundException.class,
+            NoResourceFoundException.class
+    })
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handlePageNotFoundException(Model model) {
+
+        model.addAttribute("pageTitle", "Página no encontrada");
+        model.addAttribute("errorMessage", "La página que estás buscando no existe.");
+
+        return "error/not-found";
+    }
+
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(
             Exception exception,
             Model model) {
