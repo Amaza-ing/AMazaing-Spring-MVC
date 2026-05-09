7e39f678ba9653fa0801eb8f166e3c426563ae7f
Adrián Maza <adri_mv1@hotmail.com>
2026-05-09 12:56:27 +0200

    CourseService updated with CourseDao

--- src/main/java/com/amazaing/myproject/service/CourseService.java
+++ src/main/java/com/amazaing/myproject/service/CourseService.java
@@ -1,38 +1,29 @@
 package com.amazaing.myproject.service;
 
+import com.amazaing.myproject.dao.CourseDao;
 import com.amazaing.myproject.model.Course;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CourseService {
 
-    private final List<Course> courses = new ArrayList<>();
+    private final CourseDao courseDao;
 
-    private Long nextId = 4L;
-
-    public CourseService() {
-        courses.add(new Course(1L, "HTML & CSS", "Inicial", 20));
-        courses.add(new Course(2L, "Java & POO", "Intermedio", 15));
-        courses.add(new Course(3L, "Spring MVC", "Avanzado", 25));
+    public CourseService(CourseDao courseDao) {
+        this.courseDao = courseDao;
     }
 
     public List<Course> findAll() {
-        return courses;
+        return courseDao.findAll();
     }
 
     public Course findById(Long id) {
-        return courses.stream()
-                .filter(course -> course.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+        return courseDao.findById(id);
     }
 
     public void save(Course course) {
-        course.setId(nextId);
-        courses.add(course);
-        nextId++;
+        courseDao.save(course);
     }
 }
