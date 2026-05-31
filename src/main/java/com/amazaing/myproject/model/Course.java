0fa373bc7d39086354d1aaec43992b902c4eac63
Adrián Maza <adri_mv1@hotmail.com>
2026-05-31 13:22:26 +0200

    15 lombok

--- src/main/java/com/amazaing/myproject/model/Course.java
+++ src/main/java/com/amazaing/myproject/model/Course.java
@@ -1,7 +1,13 @@
 package com.amazaing.myproject.model;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Course {
 
     private Long id;
@@ -17,46 +23,4 @@ public class Course {
     @Min(value = 1, message = "{course.duration.min}")
     @Max(value = 200, message = "{course.duration.max}")
     private Integer durationInHours;
-
-    public Course() {
-    }
-
-    public Course(Long id, String title, String level, Integer durationInHours) {
-        this.id = id;
-        this.title = title;
-        this.level = level;
-        this.durationInHours = durationInHours;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getLevel() {
-        return level;
-    }
-
-    public Integer getDurationInHours() {
-        return durationInHours;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setLevel(String level) {
-        this.level = level;
-    }
-
-    public void setDurationInHours(Integer durationInHours) {
-        this.durationInHours = durationInHours;
-    }
 }
